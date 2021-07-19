284e60742d220b8cd81143c4f38792c2ea66ac08
NioGreek <GreekNio@gmail.com>
2021-07-19 18:35:10 +0530

    wrote tests and removed modify()

--- clashgap/clashgap.py
+++ clashgap/clashgap.py
@@ -3,11 +3,8 @@ from _gap import gap
 
 class clash:
     def __init__(self, clash):
-        self._clash = gap(clash)
-    
-    def modify(self, new_clash)
-        self._clash = gap(new_clash)
-        
+        self._gap = gap(clash)
+
     def gap(self):
-        return self._clash
+        return self._gap
 
--- tests/test_clashgap.py
+++ tests/test_clashgap.py
@@ -1,5 +1,27 @@
-from clashgap import __version__
-
+import pytest
+from clashgap import *
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.0'   
+
+class clash:
+    def __init__(self):
+        clash = clash(["ham", "spam"])
+    
+    def test_gap(self):
+        assert self.clash.gap() == [['h', 'sp'], 'am']
+
+class gap:
+    def test_listHas():
+        arr = [1, 2]
+        assert list_has(arr, 1)
+        assert not(list_has(arr, 2))
+    
+    def test_maxElem():
+        arr = ["ham", "spam"]
+        assert max_elem(arr) == 1
+    
+    def test_gap():
+        arr = ["ham", "spam"]
+        assert gap(arr) == [['h', 'sp'], 'am']
+
