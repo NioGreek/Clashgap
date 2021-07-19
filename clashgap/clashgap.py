8d4c10982d7921f125e1616fa89b22583b1bce86
NioGreek <GreekNio@gmail.com>
2021-07-19 12:48:46 +0530

    implemented gap algorithm
    Tests pending

--- clashgap/clashgap.py
+++ clashgap/clashgap.py
@@ -2,8 +2,12 @@ from __init__ import __version__
 from _gap import gap
 
 class clash:
-    def __init__(self, poles):
-        self._clash = gap(poles)
-
+    def __init__(self, clash):
+        self._clash = gap(clash)
+    
+    def modify(self, new_clash)
+        self._clash = gap(new_clash)
+        
     def gap(self):
         return self._clash
+
