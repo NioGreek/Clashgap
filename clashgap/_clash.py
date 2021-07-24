54e3c6a1e4c98920995f03b5cc9d2ff4afcc49b3
John Anchery <john.anchery@gmx.com>
2021-07-24 14:40:09 +0530

    avoid code collisions

--- clashgap/_clash.py
+++ clashgap/_clash.py
@@ -1,15 +1,15 @@
-from ._gap import gap
-from ._fill import fill
+from ._gap import gap as gap_of
+from ._fill import fill as fill_of
 
 class Clash:
     def __init__(self, clash):
-        self._gap = gap(clash)
+        self._gap = gap_of(clash)
 
     def gap(self):
         return self._gap
 
     def fill(self):
-        return fill(self._gap)
+        return fill_of(self._gap)
 
     def __str__(self):
         return str(self._gap)
