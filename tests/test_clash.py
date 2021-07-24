d4324b03d6ed4d7ae39a0537dd2f2d595f763760
John Anchery <john.anchery@gmx.com>
2021-07-24 14:52:21 +0530

    maintain order

--- tests/test_clash.py
+++ tests/test_clash.py
@@ -4,11 +4,11 @@ clash = cg.Clash(["spam", "ham"])
 def test_gap():
     assert clash.gap() == [['sp', 'h'], 'am']
 
+def test_fill():
+    assert clash.fill() == ["spam", "ham"]
+
 def test_str():
     assert str(clash) == "[['sp', 'h'], 'am']"
 
 def test_repr():
     assert repr(clash) == "[['sp', 'h'], 'am']"
-
-def test_fill():
-    assert clash.fill() == ["spam", "ham"]
